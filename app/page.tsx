bc89aa87b2de41021cdf3423455b6b0e1c61430f
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2024-08-13 21:09:52 +0200

    hide navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Aurora } from "@/components/layout/aurora"
 import { AuroraBackground } from "@/components/layout/aurora-background"
 import { MeteorsCard } from "@/components/layout/meteors-card"
 import { MovingCards } from "@/components/layout/moving-cards"
-import { Navbar } from "@/components/layout/navbar"
 import { ContactForm } from "@/components/layout/contact-form"
 import { ContactAddress } from "@/components/layout/contact-address"
 
@@ -10,7 +9,8 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
       <AuroraBackground>
-        <Navbar />
+        {/* TODO: */}
+        {/* <Navbar /> */}
         <Aurora />
         <MovingCards />
         <MeteorsCard />
