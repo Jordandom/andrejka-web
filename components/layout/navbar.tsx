5a85577ce35ecc1bcf537e9097987d3105375345
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2025-07-12 15:13:38 +0200

    wip

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -18,7 +18,7 @@ function NavbarComponent({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-20", className)}
     >
       <Menu setActive={setActive}>
-        <HoveredLink href="/">Home</HoveredLink>
+        <HoveredLink href="/">O mně</HoveredLink>
         <HoveredLink href="/homeopatie">Homeopatie</HoveredLink>
 
         {/* <MenuItem setActive={setActive} active={active} item="Homeopatie">
